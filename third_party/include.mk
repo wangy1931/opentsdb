42a1609b6e737a75034d047e730d6d8ca10eb93d
Yalei Wang <wangy1931@gmail.com>
2016-03-25 15:40:50 -0700

    add build for SSL related in make

--- third_party/include.mk
+++ third_party/include.mk
@@ -28,6 +28,7 @@ include third_party/junit/include.mk
 include third_party/logback/include.mk
 include third_party/mockito/include.mk
 include third_party/netty/include.mk
+include third_party/netty-tcnative/include.mk
 include third_party/objenesis/include.mk
 include third_party/powermock/include.mk
 include third_party/protobuf/include.mk
